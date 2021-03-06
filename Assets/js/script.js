4cd650e26a821819c435912426322ba9bae99ac9
Thom Williams <thomwilliams1990@gmail.com>
2021-03-06 20:56:17 +0000

    JS: Pseudocode added

---
+++ Assets/js/script.js
@@ -0,0 +1,36 @@
+/* psuedo code...
+
+- START BUTTON pressed (click event listener)
+    - Timer Starts
+    - Question prompted with multiple choice questions. 
+    - if incorrect answers, then time deducted
+    - User inputs correct answer, new question prompted
+    REPEAT
+    - Question prompted with multiple choice questions. 
+    - if incorrect answers, then time deducted
+    - User inputs correct answer, new question prompted
+ 
+- END OF QUIZ
+ - Quiz Over prompt when all questions completed
+ - OR Timer equals zero
+
+
+ - ENTER SCORE
+ - Score presented in prompt 
+ - Enter initials window 
+ - Try again....
+
+
+ VARIABLES 
+ - Array of Quiz questions
+ - Array of answers for each question
+ - right answer
+ - wrong answers
+ - timer start
+ - timercountdown
+ - win message
+ - lose message
+ - score
+ - enter intials user input
+ - reset button
+ 
