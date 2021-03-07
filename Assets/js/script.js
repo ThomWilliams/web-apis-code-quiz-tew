328d287b7a0965f205af2ee5ed880e9530bc1fb9
Thom Williams <thomwilliams1990@gmail.com>
2021-03-07 11:48:22 +0000

    JS: Timer Element Added

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,59 @@
+var highScore = document.querySelector(".highscore");
+var timeEl = document.querySelector(".timer");
+var quizStart = document.querySelector(".quizstart");
+var questionGenerator = document.querySelector(".questiongen");
+var qAnswers = document.querySelector(".answers");
+var a1 = document.querySelector(".a1");
+var a2 = document.querySelector(".a2");
+var a3 = document.querySelector(".a3");
+var a4 = document.querySelector(".a4");
+
+var correctAnswer = 0;
+var incorrectAnswer = 0;
+var timer;
+var timerCount = 10;
+
+// Start Quiz Event Listener
+quizStart.addEventListener("click", startTimer);
+
+
+// Timer function
+function startTimer () {
+    timer = setInterval(function() {
+        timerCount--;
+        timeEl.textContent = timerCount + " seconds remaining";
+        // clock hits zero, reset
+        if (timerCount === 0) {
+            timeEl.textContent = "Out of Time";
+         clearInterval(timer);
+        }
+    }, 1000);
+}
+
+
+// Question One 
+// function question1 () {
+
+//     var question1 = "This Is Question One....";
+//     a1 = "answer: correct";
+//     a2 = "answer: incorrect";
+//     a3 = "answer: incorrect";
+//     a4 = "answer: incorrect";
+
+//     if question1 = correct (a1) {
+//         correctAnswer++
+//         //correct
+//     }
+
+//     if question1 = incorrect (a2, a3, a4,) {
+//         incorrectAnswer--
+//         // incorrect
+//     }
+
+// }
+
+
+
 /* psuedo code...
 
 - START BUTTON pressed (click event listener)
@@ -32,5 +88,5 @@
  - lose message
  - score
  - enter intials user input
- - reset button
- 
+ - reset button */
+
